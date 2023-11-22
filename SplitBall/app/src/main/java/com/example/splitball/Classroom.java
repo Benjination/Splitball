7fdf9a62d939752127e539e32b381f43f769f293
rumaysajafer <mxshuu192@gmail.com>
2023-11-22 16:35:45 -0600

    made changes to add pause button and to pause screen

--- SplitBall/app/src/main/java/com/example/splitball/Classroom.java
+++ SplitBall/app/src/main/java/com/example/splitball/Classroom.java
@@ -2,7 +2,10 @@ package com.example.splitball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Classroom extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Classroom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classroom);
+
+        Button pause = findViewById(R.id.pause);
+
+        pause.setOnClickListener(v ->{
+                Intent intent = new Intent(Classroom.this, PauseScreen.class);
+                startActivity(intent);
+            });
     }
 }
--- SplitBall/app/src/main/java/com/example/splitball/PauseScreen.java
+++ SplitBall/app/src/main/java/com/example/splitball/PauseScreen.java
@@ -2,7 +2,9 @@ package com.example.splitball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PauseScreen extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class PauseScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pause_screen);
+
+        Button backToMain = findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(v -> {
+            Intent intent = new Intent(PauseScreen.this, MainMenu.class);
+            startActivity(intent);
+        });
+        //settings, exit, resume, restart
+        //Button restartButton = findViewById(R.id.restartButton);
+        //Button exitButton = findViewById(R.id.exitButton);
+        //Button resumeButton = findViewById(R.id.resumeButton);
     }
 }
