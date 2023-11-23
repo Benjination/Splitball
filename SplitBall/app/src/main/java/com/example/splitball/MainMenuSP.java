6e626f54afa7a2a0e4cd57aad7af3925c81ab3f5
Rumaysa Jafer <mxshuu198@gmail.com>
2023-11-22 22:04:05 -0600

    for sp

--- SplitBall/app/src/main/java/com/example/splitball/MainMenuSP.java
+++ SplitBall/app/src/main/java/com/example/splitball/MainMenuSP.java
@@ -14,26 +14,26 @@ public class MainMenuSP extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu_sp);
 
         Button settings = findViewById(R.id.ajustes);
-
         settings.setOnClickListener(v -> {
             Intent intent = new Intent(MainMenuSP.this, SettingsSP.class);
             startActivity(intent);
         });
+
         Button classroom = findViewById(R.id.classroomSP);
 
         classroom.setOnClickListener(v -> {
             Intent intent = new Intent(MainMenuSP.this, Classroom.class);
             startActivity(intent);
         });
-        Button leaderboard = findViewById(R.id.leaderboardSP);
 
+        Button leaderboard = findViewById(R.id.leaderboardSP);
         leaderboard.setOnClickListener(v -> {
             Intent intent = new Intent(MainMenuSP.this, LeaderboardSP.class);
             startActivity(intent);
         });
-        Button connectToSocial = findViewById(R.id.connectToSocialSP);
 
-        connectToSocial.setOnClickListener(v -> {
+        Button socialsp = findViewById(R.id.socialSP);
+        socialsp.setOnClickListener(v -> {
             Intent intent = new Intent(MainMenuSP.this, SocialSP.class);
             startActivity(intent);
         });
