6b2baf9ed1693c297244f9aa31990128bc258bbc
Benjamin Niccum <btn1609@mavs.uta.edu>
2023-11-26 15:27:24 -0600

    Player score tallies and ammo decreases when ammo images are pressed.

--- SplitBall/app/src/main/java/com/example/splitball/Classroom.java
+++ SplitBall/app/src/main/java/com/example/splitball/Classroom.java
@@ -11,33 +11,80 @@ import android.widget.TextView;
 
 public class Classroom extends AppCompatActivity {
 
-
+    long[] score = new long[1];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classroom);
 
+        score[0] = 0;
+
         Button pause = findViewById(R.id.pause);
+        ImageView paperball, airplane, star, kunai;
+        paperball = findViewById(R.id.paperball);
+        airplane = findViewById(R.id.airplane);
+        star = findViewById(R.id.star);
+        kunai = findViewById(R.id.kunai);
 
-        int ammoNumP =0, ammoNumA=1, ammoNumS=2, ammoNumK=3, lives=3;
-        double highScore = 0;
+        int lives = 3;
+        int[]  ammoNum = new int[4];
+        ammoNum[0] = 0;  //paperball
+        ammoNum[1] = 1;  //airplane
+        ammoNum[2] = 2;  //star
+        ammoNum[3] = 3;  //kunai
 
+
+        TextView scoreView = findViewById(R.id.score);
+        scoreView.setText("SCORE: " + score[0]);
         TextView ammoPaper = findViewById(R.id.ammoNumP);
-        ammoPaper.setText("X " + ammoNumP);
+        ammoPaper.setText("X " + ammoNum[0]);
         TextView ammoAirplane = findViewById(R.id.ammoAirplane);
-        ammoAirplane.setText("X " + ammoNumA);
+        ammoAirplane.setText("X " + ammoNum[1]);
         TextView ammoStar = findViewById(R.id.ammoStar);
-        ammoStar.setText("X " + ammoNumS);
+        ammoStar.setText("X " + ammoNum[2]);
         TextView ammoKunai = findViewById(R.id.ammoKunai);
-        ammoKunai.setText("X " + ammoNumK);
+        ammoKunai.setText("X " + ammoNum[3]);
 
         pause.setOnClickListener(v ->{
             Intent intent = new Intent(Classroom.this, PauseScreen.class);
             startActivity(intent);
         });
-
-
-
+        paperball.setOnClickListener(v ->{
+           //trigger paperball firing animation
+            if(ammoNum[0] != 0) {
+                ammoNum[0]--;
+                score[0] += 100;
+                scoreView.setText("SCORE: " + score[0]);
+                ammoPaper.setText("X " + ammoNum[0]);
+            }
+        });
+        airplane.setOnClickListener(v ->{
+            //trigger airplane firing animation
+            if(ammoNum[1] != 0){
+                ammoNum[1]--;
+                score[0] += 150;
+                scoreView.setText("SCORE: " + score[0]);
+                ammoAirplane.setText("X " + ammoNum[1]);
+            }
+        });
+        star.setOnClickListener(v ->{
+            //trigger ninja star firing animation
+            if(ammoNum[2] != 0) {
+                ammoNum[2]--;
+                score[0] += 200;
+                scoreView.setText("SCORE: " + score[0]);
+                ammoStar.setText("X " + ammoNum[2]);
+            }
+        });
+        kunai.setOnClickListener(v ->{
+            //trigger kunai firing animation
+            if(ammoNum[3] != 0) {
+                ammoNum[3]--;
+                score[0] += 500;
+                scoreView.setText("SCORE: " + score[0]);
+                ammoKunai.setText("X " + ammoNum[3]);
+            }
+        });
 
     }
     public void BombIsPressed(int lives) {
@@ -64,6 +111,5 @@ public class Classroom extends AppCompatActivity {
                 System.out.println("Lives should not be outside of the range 0-3. An error has occured.");
                 break;
         }
-
     }
 }
